adfae5c1589863a36a3251a2abb0a7ffe6a58af4
Alessandro Solbiati <lessandro@fb.com>
2020-05-17 23:16:06 +0100

    [2/2] setup package, uploaded to https://test.pypi.org/project/xbot/0.0.1/

---
+++ xbot/__main__.py
@@ -0,0 +1,12 @@
+"""xbot entrypoint"""
+if __name__ == "__main__":
+    print('this is main')
+    import argparse
+    parser = argparse.ArgumentParser(description='Add some integers.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                        help='interger list')
+    parser.add_argument('--sum', action='store_const',
+                        const=sum, default=max,
+                        help='sum the integers (default: find the max)')
+    args = parser.parse_args()
+    print(args.sum(args.integers))
